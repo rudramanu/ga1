a1db9a3611d7344f286df522a88421441faaf6c6
Rudra <merudramanu@gmail.com>
2023-06-28 13:16:19 +0530

    added accurate prompt

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/generateResponse", async (req, res) => {
     }
     console.log("hello");
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: `Shayari in Hindi about ${prompt}`,
     });
     console.log("hi");
